6d417d2d11624bd96b7ebf5d4599089009311bbf
Peter Meehan <peter.meehan@intercom.io>
2019-10-11 00:22:38 +0100

    change classicDecorator default

--- transforms/ember-object/index.js
+++ transforms/ember-object/index.js
@@ -5,7 +5,7 @@ const { replaceEmberObjectExpressions } = require('../helpers/parse-helper');
 const DEFAULT_OPTIONS = {
   decorators: true,
   classFields: true,
-  classicDecorator: true,
+  classicDecorator: false, // this doesn't want to read the command line arg :(
   quote: 'single',
 };
 
